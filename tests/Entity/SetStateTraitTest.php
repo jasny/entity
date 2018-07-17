020c3cdc00a7c9b9519095fea79d5045f63236aa
Sergey Boltonosov <minstel@yandex.ru>
2018-07-17 22:58:24 +0400

    Complete tests for SetStateTrait

--- tests/Entity/SetStateTraitTest.php
+++ tests/Entity/SetStateTraitTest.php
@@ -12,6 +12,9 @@ use PHPUnit\Framework\TestCase;
  */
 class SetStateTraitTest extends TestCase
 {
+    /**
+     * Test '__set_state' method for non-dynamic entity
+     */
     public function testSetState()
     {
         $entity = TestEntity::__set_state(['foo' => 'bar', 'num' => 22, 'dyn' => 'woof']);
@@ -24,6 +27,9 @@ class SetStateTraitTest extends TestCase
         $this->assertObjectNotHasAttribute('dyn', $entity);
     }
 
+    /**
+     * Test '__set_state' method for dynamic entity
+     */
     public function testSetStateWithDynamicEntity()
     {
         $entity = DynamicTestEntity::__set_state(['foo' => 'bar', 'num' => 22, 'dyn' => 'woof']);
@@ -35,6 +41,9 @@ class SetStateTraitTest extends TestCase
         $this->assertAttributeSame('woof', 'dyn', $entity);
     }
 
+    /**
+     * Test '__set_state' method when constructor should be called
+     */
     public function testSetStateConstruct()
     {
         $entity = TestEntity::__set_state([]);
@@ -42,5 +51,19 @@ class SetStateTraitTest extends TestCase
         $this->assertInstanceOf(TestEntity::class, $entity);
 
         $this->assertAttributeSame(0, 'num', $entity);
+    }
+
+    /**
+     * Test 'markAsPersisted' method
+     */
+    public function testMarkAsPersisted()
+    {
+        $entity = $this->createPartialMock(TestEntity::class, ['trigger']);
+        $entity->expects($this->once())->method('trigger')->with('persisted');
+
+        $result = $entity->markAsPersisted();
+
+        $this->assertSame($entity, $result);
+        $this->assertFalse($entity->isNew());
     }
 }
