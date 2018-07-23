e8cc11d602cc45f0c3d5930e747aa615c73dd9fd
Sergey Boltonosov <minstel@yandex.ru>
2018-07-23 23:05:51 +0400

    Tests for collection SortTrait

--- src/EntityCollection/Traits/SortTrait.php
+++ src/EntityCollection/Traits/SortTrait.php
@@ -22,14 +22,26 @@ trait SortTrait
      * @param string $property
      * @param int    $sortFlags
      * @return $this
+     * @throws BadMethodCallException If $property param is null and __toString() method is not implemented in entity class
      */
     public function sort(string $property = null, int $sortFlags = SORT_REGULAR)
     {
         $index = [];
         $entities = [];
+        $useToString = false;
+
+        if (!isset($property)) {
+            if (!method_exists($this->entityClass, '__toString')) {
+                throw new BadMethodCallException("Class {$this->entityClass} does not have __toString method, to use it for sorting");
+            }
+
+            $useToString = true;
+        }
 
         foreach ($this->entities as $key => $entity) {
-            $index[$key] = $entity->$property ?? null;
+            $index[$key] = $useToString ?
+                (string)$entity :
+                ($entity->$property ?? null);
         }
 
         asort($index, $sortFlags);
