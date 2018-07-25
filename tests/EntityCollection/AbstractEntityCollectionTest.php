80e927d1ee63ffbfc3f09df48622ffddd345058c
Sergey Boltonosov <minstel@yandex.ru>
2018-07-25 10:45:24 +0400

    Tests for AbstractEntityCollection

---
+++ tests/EntityCollection/AbstractEntityCollectionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Jasny\DB;
+
+use PHPUnit\Framework\TestCase;
+use Jasny\EntityCollection\AbstractEntityCollection;
+
+/**
+ * @covers Jasny\EntityCollection\AbstractEntityCollection
+ */
+class AbstractEntityCollectionTest extends TestCase
+{
+    use \Jasny\TestHelper;
+
+    /**
+     * Test 'getEntityClass' method
+     */
+    public function testGetEntityClass()
+    {
+        $class = AbstractEntityCollection::class;
+        $collection = (new \ReflectionClass($class))->newInstanceWithoutConstructor();
+
+        $this->setPrivateProperty($collection, 'entityClass', 'FooEntity');
+
+        $result = $collection->getEntityClass();
+
+        $this->assertSame('FooEntity', $result);
+    }
+}
